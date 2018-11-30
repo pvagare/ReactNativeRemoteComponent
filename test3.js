065175984b7c5de47650ec2472cf6d6bbbbf7ebc
Pankaj Vagare <pvagare@genius-minds.com>
2018-12-01 00:10:13 +0530

    Update test3.js

--- test3.js
+++ test3.js
@@ -7,11 +7,11 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _react = require('react');
+var _react = React;
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactNative = require('react-native');
+var _reactNative = React;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
